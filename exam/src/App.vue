5cfa5e2fe1ed3226ec894cfdf6a295a654f1fe4e
geramos2004-codificador <geramos.2004@gmail.com>
2025-09-30 12:07:25 -0600

    agregue al formulario nombre y apellido

--- exam/src/App.vue
+++ exam/src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <h1>Formulario</h1>
+
+ <div>
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" required>
+        
+        <label for="apellido">Apellido:</label>
+        <input type="text" id="apellido" name="apellido" placeholder="Tu apellido" required>
+    </div>
+
 </template>
 
 <script>
