244e2c9e7cae283e853577d5ed9a7b15a1bd63c6
geramos2004-codificador <geramos.2004@gmail.com>
2025-09-30 12:06:35 -0600

    agregue titulo Formulario

--- exam/src/App.vue
+++ exam/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <h1>Formulario</h1>
 </template>
 
 <script>
@@ -7,12 +7,5 @@
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
