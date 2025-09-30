a13385319b9b6b40e8227b5770ba6813ba56b490
geramos2004-codificador <geramos.2004@gmail.com>
2025-09-30 12:32:17 -0600

    cambio en nombres de formulario

--- exam/src/App.vue
+++ exam/src/App.vue
@@ -8,6 +8,7 @@
       <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" required>
 
       <label for="apellido">Apellido:</label>
+      
       <input type="text" id="apellido" name="apellido" placeholder="Tu apellido" required>
     </div>
 
